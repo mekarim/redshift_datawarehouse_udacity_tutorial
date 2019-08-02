7dcd54f8781ce4411917e1bef1d547427064d9fd
M Ehsanul Karim <ehsan.mce@gmail.com>
2019-08-02 21:42:45 +0000

    Implement and test DELETE TABLE functions and sql queries

--- create_tables.py
+++ create_tables.py
@@ -21,13 +21,11 @@ def main():
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-
-    #drop_tables(cur, conn)
+    
+    drop_tables(cur, conn)
     #create_tables(cur, conn)
-    print('It connects!')
+    
     conn.close()
-    print('It connects and closes!')
-
 
 if __name__ == "__main__":
     main()
